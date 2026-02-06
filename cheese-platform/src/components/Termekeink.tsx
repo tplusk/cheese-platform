2ea288dacee32f1b9c992b578646cf10201cf0a9
tplusk <macfelelhun@gmail.com>
2026-02-06 15:58:04 +0000

    add id to lists

--- cheese-platform/src/components/Termekeink.tsx
+++ cheese-platform/src/components/Termekeink.tsx
@@ -10,28 +10,30 @@ export default function Termekeink() {
       </nav>
       <div className="termekeink">
         <h2 className="termekeink">Termékeink</h2>
-        <div>
-          <ul>
-            <li>gouda sajt</li>
-            <li>öreg gouda</li>
-            <li>4hagymás gouda</li>
-            <li>füstölt-fokhagymás gouda</li>
-            <li>oregánó-bazsalikomos gouda</li>
-            <li>zöldborsos gouda</li>
-            <li>köménymagos gouda</li>
-            <li>chilis gouda</li>
-            <li>medvehagymás gouda</li>
-            <li>maasdammer sajt</li>
-          </ul>
-        </div>
-        <div>
-          <ul>
-            <li>natúr joghurt</li>
-            <li>áfonyás joghurt</li>
-            <li>meggyes joghurt</li>
-            <li>tejföl</li>
-            <li>túró</li>
-          </ul>
+        <div id="list">
+          <div>
+            <ul>
+              <li>gouda sajt</li>
+              <li>öreg gouda</li>
+              <li>4hagymás gouda</li>
+              <li>füstölt-fokhagymás gouda</li>
+              <li>oregánó-bazsalikomos gouda</li>
+              <li>zöldborsos gouda</li>
+              <li>köménymagos gouda</li>
+              <li>chilis gouda</li>
+              <li>medvehagymás gouda</li>
+              <li>maasdammer sajt</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>natúr joghurt</li>
+              <li>áfonyás joghurt</li>
+              <li>meggyes joghurt</li>
+              <li>tejföl</li>
+              <li>túró</li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
