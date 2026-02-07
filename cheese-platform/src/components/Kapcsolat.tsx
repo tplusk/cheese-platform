2bda35249b9efbe24d9e7cf39fdaf119ee691586
tplusk <macfelelhun@gmail.com>
2026-02-07 09:38:25 +0000

    update contact key nyitva

--- cheese-platform/src/components/Kapcsolat.tsx
+++ cheese-platform/src/components/Kapcsolat.tsx
@@ -5,7 +5,7 @@ export default function Kapcsolat() {
     cimunk: "6800 Hódmezővásárhely, Aranyág kert 6.",
     telefon: "62/534-586",
     email: "boppehu@gmail.com",
-    nyitva: "Hétfő - Péntek 8:00 – 15:00",
+    nyitvatartas: "Hétfő - Péntek, 8:00 – 15:00",
   };
 
   return (
@@ -20,7 +20,7 @@ export default function Kapcsolat() {
         <h3 id="cimunk">Címünk: {contact.cimunk}</h3>
         <h3 id="telefon">Telefon: {contact.telefon}</h3>
         <h3 id="email">Email: {contact.email}</h3>
-        <h3 id="nyitva">Nyitvatartás: {contact.nyitva}</h3>
+        <h3 id="nyitva">Nyitvatartás: {contact.nyitvatartas}</h3>
       </footer>
     </>
   );
