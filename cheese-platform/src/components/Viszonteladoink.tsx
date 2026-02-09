d79890fb51ea27054629446583b52be924dfaf80
tplusk <macfelelhun@gmail.com>
2026-02-09 11:37:41 +0000

    update hmvhely, szeged, bp arrays

--- cheese-platform/src/components/Viszonteladoink.tsx
+++ cheese-platform/src/components/Viszonteladoink.tsx
@@ -2,31 +2,31 @@ import { Link } from "react-router-dom";
 
 export default function Viszonteladoink() {
   const hmvhely = [
-    "Nagyi Vegyeskereskedés Pálffy u.",
     "Béketelepi ABC Szent László u. 153.",
+    "Nagyi Vegyeskereskedés Pálffy u.",
     "Pörkölt bolt Lehel u. 6.",
   ];
 
   const szeged = [
+    "CBA Príma Nova, Újszeged, Szőregi út 80.",
+    "Coop 1101-es sz. ABC - Napsugár Áruház",
+    "Dél-Gasztró KFT, Kenyérgyári út 3.",
     "Kék Boci Tejbolt, Szent István tér A pavilon",
-    "Riska tejbolt, Űrhajós utca 13/A.",
-    "Zöldség-gyümölcs Bolt, Molnár utca 3-5.",
-    "Lipóti Pékség, Széchenyi tér 16.",
-    "Lipóti Pékség, (Anna-kút) Tisza Lajos körút 39.",
     "Lipóti Pékség, Algyői út 72. (Lukoil mellett)",
+    "Lipóti Pékség, (Anna-kút) Tisza Lajos körút 39.",
+    "Lipóti Pékség, Széchenyi tér 16.",
     "Lipóti Pékség, Derkovits Fasor 17.",
     "Nóri Csemegéje, Mars tér / Irén csarnok",
-    "CBA Príma Nova, Újszeged, Szőregi út 80.",
-    "Reál ABC, Fő fasor 100.",
-    "Dél-Gasztró KFT, Kenyérgyári út 3.",
-    "Coop 1101-es sz. ABC - Napsugár Áruház",
     "Pizza Box KFT. (online rendelés, csak kiszállítás) tehéntej.hu",
+    "Reál ABC, Fő fasor 100.",
+    "Riska tejbolt, Űrhajós utca 13/A.",
+    "Zöldség-gyümölcs Bolt, Molnár utca 3-5.",
   ];
 
   const bp = [
-    "Szega Camembert Sajtszaküzlet -Budagyöngye Bevásárlóközpont -I. szint 1026 Budapest, Szilágyi Erzsébet Fasor 121.",
     "Fény utcai piac 1024 Budapest, Lövőház u. 12. 4. em. 27-es üzlet",
     "Rózsakert Bevásárlóközpont, 1023 Budapest, Gábor Áron u. 74/A",
+    "Szega Camembert Sajtszaküzlet -Budagyöngye Bevásárlóközpont -I. szint 1026 Budapest, Szilágyi Erzsébet Fasor 121.",
   ];
 
   return (
