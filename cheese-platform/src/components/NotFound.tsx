bddd6bb8357daf2c990571de3168681731e3abcd
tplusk <macfelelhun@gmail.com>
2026-02-10 13:40:50 +0000

    add link back to Cimlap

--- cheese-platform/src/components/NotFound.tsx
+++ cheese-platform/src/components/NotFound.tsx
@@ -1,7 +1,10 @@
 export default function NotFound() {
   return (
     <div>
-      <h2 className="notFound">404 - Page Not found!</h2>;
+      <h2 className="notFound">404 - A keresett oldal nem található!</h2>;
+      <a className="notFound" href="/">
+        Vissza a Címlapra
+      </a>
     </div>
   );
 }
